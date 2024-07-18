8f320c2566b9f379ea9cd054c0a3bc06be6960c1
Stefano Tumino <47145720+Nefasto1@users.noreply.github.com>
2024-07-18 17:29:26 +0200

    Update DQN.py

--- src/DQN.py
+++ src/DQN.py
@@ -5,16 +5,18 @@ class DQN(th.nn.Module):
         super(DQN, self).__init__()
         
         self.linear = th.nn.Sequential(
-                          th.nn.Linear(in_features=7, out_features=7,      bias=True),
+                          th.nn.Linear(in_features=7, out_features=128,      bias=True),
                           th.nn.ReLU(),
-                          th.nn.Linear(in_features=7, out_features=7,      bias=True),
+                          th.nn.Linear(in_features=128, out_features=128,      bias=True),
                           th.nn.ReLU(),
-                          th.nn.Linear(in_features=7, out_features=3 * 11, bias=True),
+                          th.nn.Linear(in_features=128, out_features=4, bias=True),
+                          # th.nn.Linear(in_features=128, out_features=11 ** 3, bias=True),
                       )
 
     def forward(self, x):
         out = self.linear(x)
 
-        out = out.reshape(-1, 3, 11)
-        return th.softmax(out, axis=1)
+        # out = out.reshape(-1, 3, 11)
+        # out = out.reshape(-1, 11, 11, 11)
+        return out
 
