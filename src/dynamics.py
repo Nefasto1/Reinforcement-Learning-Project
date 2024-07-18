231483504ba6ee4e16b19675757be3f31a5cab86
Alessandro Della Siega <adellasiega@login01.hpc.rd.areasciencepark.it>
2024-07-18 14:57:53 +0000

    fixed dynamics

--- src/dynamics.py
+++ src/dynamics.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-def integrate_dynamic_step(vel_0, x_0, acc,
-                           dt):
+def integrate_dynamic_step(vel_0, x_0, acc, dt):
         
         vel = vel_0 + acc * dt
         x = x_0 + vel * dt
 
         return vel, x
+
+def distance(x1, x2):
+    
+    d = np.sqrt(np.sum(((x1-x2))**2))
+    
+    return d
+    
 
 def dynamic_step(F1_mod: float, F2_mod:float, F3_mod: float, 
                  alpha1: float, alpha2: float, alpha3: float,
@@ -25,40 +31,52 @@ def dynamic_step(F1_mod: float, F2_mod:float, F3_mod: float,
         
         F3 = F3_mod * np.array([np.cos(alpha3 * np.pi/180),
                                 np.sin(alpha3 * np.pi/180)])
+
+        #Constants
+        G                   = 6.674 * 1e-11 # N m^2 kg^-2
+        mass_moon           = 7.342 * 1e20  # kg
+        mass_earth          = 5.972 * 1e24  # kg
+        pixel_side          = 3.624 * 1e5   # m
         
-        distance_shuttle_moon = np.sqrt(np.sum((com_0 - moon_coords)**2))
-        distance_shuttle_earth = np.sqrt(np.sum((com_0 - earth_coords)**2))
+        distance_shuttle_moon  = distance(com_0 * pixel_side, moon_coords * pixel_side)
+        distance_shuttle_earth = distance(com_0 * pixel_side, earth_coords * pixel_side)
 
-        G = 6.674*1e-11 # Gravitational constant
-        unit_vector_shuttle_moon = (com_0 - moon_coords)/distance_shuttle_moon
-        unit_vector_shuttle_earth = (com_0 - earth_coords)/distance_shuttle_moon
+        F_grav_moon = G * mass_shuttle * mass_moon / distance_shuttle_moon**2
+        beta = np.arccos((moon_coords - com_0)[1]*pixel_side/distance_shuttle_moon)
+        F_grav_moon_x = F_grav_moon * np.sin(beta)
+        F_grav_moon_y = F_grav_moon * np.cos(beta)
+        
+        F_grav_earth = G * mass_shuttle * mass_earth / distance_shuttle_earth**2
+        gamma = np.arccos((earth_coords-com_0)[1]*pixel_side/distance_shuttle_earth)
+        F_grav_earth_x = F_grav_earth * np.sin(gamma)
+        F_grav_earth_y = F_grav_earth * np.cos(gamma)
 
-        mass_moon = 7.342*1e22
-        mass_earth = 5.972*1e24
+        F_grav = np.array((F_grav_moon_x + F_grav_earth_x,
+                           F_grav_moon_y + F_grav_earth_y))
+    
+        
 
-        F_grav = G * mass_shuttle * (mass_moon  * unit_vector_shuttle_moon  / (distance_shuttle_moon *32000)**2 * (distance_shuttle_moon  > 80) * (distance_shuttle_moon  < 200)
-                                  +  mass_earth * unit_vector_shuttle_earth / (distance_shuttle_earth*32000)**2 * (distance_shuttle_earth > 140) * (distance_shuttle_earth < 400))
-
-        F_net = F1*400 + F2*800 + F3*400 + F_grav
+                                         
+        F_net = 400*F1 + 800*F2 + 400*F3 + 400*F_grav
+        print(F_grav/F_net *  100)
 
         tau_net = np.sum([r1[0]*F1[1] - r1[1]*F1[0],
                           r2[0]*F2[1] - r2[1]*F2[0],
                           r3[0]*F3[1] - r3[1]*F3[0]])
 
-        I = mass_shuttle * (width**2 + height**2) / 12
-        
-        theta_acc = tau_net / 1 #/ I
-        com_acc = F_net / 100000*mass_shuttle
+        I = (width**3 + height) / 12
 
-        theta_vel, theta = integrate_dynamic_step(theta_vel_0, theta_0, theta_acc,
-                                                                dt)
+        theta_acc = tau_net / I
+        com_acc = F_net / mass_shuttle
+
+        theta_vel, theta = integrate_dynamic_step(theta_vel_0, theta_0, theta_acc, dt)
     
         com_acc_projected =  com_acc * np.array([np.cos(theta * np.pi/180),
                                                  np.sin(theta * np.pi/180)])
-        com_acc_projected +=  com_acc[::-1] * np.array([np.cos(theta * np.pi/180),
-                                                 np.sin(theta * np.pi/180)])
+        
+        com_acc_projected += com_acc[::-1] * np.array([np.cos(theta * np.pi/180),
+                                                       np.sin(theta * np.pi/180)])
     
-        com_vel, com = integrate_dynamic_step(com_vel_0, com_0, com_acc_projected,
-                                                                dt)
+        com_vel, com = integrate_dynamic_step(com_vel_0, com_0, com_acc_projected, dt)
 
         return theta_vel, theta, com_vel, com
